b48ae3f2298c2fbc3d2839b140c7fc15426cb787
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2023-04-17 12:54:39 +0530

    Add files via upload

---
+++ functions/functions.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+// void print_name()
+// {
+//     int n;
+//     cout<<"Enter A Value";
+//     cout<<endl;
+//     cin>> n;
+//     for (int i = 0; i < n ; i++)
+//     {
+//         cout<<"Harsh";
+//         cout<<endl;
+//     }
+    
+// }
+void PrintNum(int num){
+    cout<<num<<endl;
+}
+int main()
+{
+//     print_name();
+int a =5;
+cout<<"address of a is:" << &a <<endl;
+    // PrintNum(a);
+    return 0;
+}
